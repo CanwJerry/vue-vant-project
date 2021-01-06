081ca1b3c151a00e9844b702f7f134ec296232ea
canwei.wu <canwei.wu@southgis.com>
2021-01-06 15:10:58 +0800

    调整端口号

--- vue.config.ts
+++ vue.config.ts
@@ -48,7 +48,7 @@ module.exports = {
   devServer: {
     open: process.platform === 'darwin',
     host: '0.0.0.0',
-    port: 7070,
+    port: 8888,
     https: false,
     hotOnly: false,
     proxy: null, // 设置代理
